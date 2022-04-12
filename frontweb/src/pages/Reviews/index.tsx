e467b22540ce8199c161efe96557cba6b1c68e94
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-04-12 14:19:21 -0300

    Content restriction

--- frontweb/src/pages/Reviews/index.tsx
+++ frontweb/src/pages/Reviews/index.tsx
@@ -1,6 +1,5 @@
-
-
-import ReviewCard from "./ReviewCard";
+import { hasAnyRoles } from 'utils/auth';
+import ReviewCard from './ReviewCard';
 import './styles.css';
 
 const Reviews = () => {
@@ -10,30 +9,32 @@ const Reviews = () => {
         <h1>Tela detalhes do filme id: 1</h1>
       </div>
 
-      <div className="input-card base-card">
-        <form>
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              name="text"
-              placeholder="Deixe sua avaliação aqui"
-            />
-          </div>
+      {hasAnyRoles(['ROLE_MEMBER']) && (
+        <div className="input-card base-card">
+          <form>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="text"
+                placeholder="Deixe sua avaliação aqui"
+              />
+            </div>
 
-          <button type="submit" className="btn btn-primary">
-            SALVAR AVALIAÇÃO
-          </button>
-        </form>
-      </div>
+            <button type="submit" className="btn btn-primary">
+              SALVAR AVALIAÇÃO
+            </button>
+          </form>
+        </div>
+      )}
 
       <div className="reviews-group base-card">
-        <ReviewCard/>
-        <ReviewCard/>
-        <ReviewCard/>
-        <ReviewCard/>
-        <ReviewCard/>
-        <ReviewCard/>
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
       </div>
     </div>
   );
