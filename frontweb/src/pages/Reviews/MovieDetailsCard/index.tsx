bcad8ed596af5f93b1de63b939e5198837be3b16
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-05-11 18:27:12 -0300

    MovieDetailsCard responsiveness

--- frontweb/src/pages/Reviews/MovieDetailsCard/index.tsx
+++ frontweb/src/pages/Reviews/MovieDetailsCard/index.tsx
@@ -6,24 +6,26 @@ const MovieDetailsCard = () => {
       <div className="movie-details-img">
         <img src="https://picsum.photos/260/163" alt="Foto aleatória" />
       </div>
-      <div className="movie-details-infos">
-        <h2>O Retorno do Rei</h2>
-        <p className="movie-details-year">2013</p>
-        <p className="movie-details-subtitle">
-          O olho do inimigo está se movendo.
-        </p>
-      </div>
-      <div className="movie-details-synopsis">
-        <p>
-          O confronto final entre as forças do bem e do mal que lutam pelo
-          controle do futuro da Terra Média se aproxima. Sauron planeja um
-          grande ataque a Minas Tirith, capital de Gondor, o que faz com que
-          Gandalf e Pippin partam para o local na intenção de ajudar a
-          resistência. Um exército é reunido por Theoden em Rohan, em mais uma
-          tentativa de deter as forças de Sauron. Enquanto isso, Frodo, Sam e
-          Gollum seguem sua viagem rumo à Montanha da Perdição para destruir o
-          anel.
-        </p>
+      <div className="movie-details-content">
+        <div className="movie-details-infos">
+          <h2>O Retorno do Rei</h2>
+          <p className="movie-details-year">2013</p>
+          <p className="movie-details-subtitle">
+            O olho do inimigo está se movendo.
+          </p>
+        </div>
+        <div className="movie-details-synopsis">
+          <p>
+            O confronto final entre as forças do bem e do mal que lutam pelo
+            controle do futuro da Terra Média se aproxima. Sauron planeja um
+            grande ataque a Minas Tirith, capital de Gondor, o que faz com que
+            Gandalf e Pippin partam para o local na intenção de ajudar a
+            resistência. Um exército é reunido por Theoden em Rohan, em mais uma
+            tentativa de deter as forças de Sauron. Enquanto isso, Frodo, Sam e
+            Gollum seguem sua viagem rumo à Montanha da Perdição para destruir o
+            anel.
+          </p>
+        </div>
       </div>
     </div>
   );
