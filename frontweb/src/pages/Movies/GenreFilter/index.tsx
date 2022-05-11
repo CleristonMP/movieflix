8baa6a2ebdc3b287996a814ad44fe3a5c66e3587
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-05-10 22:53:56 -0300

    Select Genres

--- frontweb/src/pages/Movies/GenreFilter/index.tsx
+++ frontweb/src/pages/Movies/GenreFilter/index.tsx
@@ -1,13 +1,68 @@
+import { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import Select from 'react-select';
+import { Genre } from 'types/genre';
+import { requestBackend } from 'utils/requests';
+
 import './styles.css';
 
-const GenreFilter = () => {
+export type GenreFilterData = {
+  genre: Genre | null;
+};
+
+type Props = {
+  onSubmitFilter: (data: GenreFilterData) => void;
+};
+
+const GenreFilter = ({ onSubmitFilter }: Props) => {
+  const [selectGenres, setSelectGenres] = useState<Genre[]>([]);
+
+  const { handleSubmit, setValue, getValues, control } =
+    useForm<GenreFilterData>();
+
+  const onSubmit = (formData: GenreFilterData) => {
+    onSubmitFilter(formData);
+  };
+
+  const handleChangeGenre = (value: Genre) => {
+    setValue('genre', value);
+
+    const obj: GenreFilterData = {
+      genre: getValues('genre'),
+    };
+
+    onSubmitFilter(obj);
+  };
+
+  useEffect(() => {
+    requestBackend({ url: '/genres', withCredentials: true }).then(
+      (response) => {
+        console.log(response.data);
+        setSelectGenres(response.data);
+      }
+    );
+  }, []);
+
   return (
     <div className="genre-filter-ctr base-card">
-      <select name="genres" id="genres">
-        <option value="action">Ação</option>
-        <option value="adventure">Aventura</option>
-        <option value="sci-fi">Ficção científica</option>
-      </select>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="genre"
+          control={control}
+          render={({ field }) => (
+            <Select
+              {...field}
+              options={selectGenres}
+              isClearable
+              placeholder="Gênero"
+              classNamePrefix="genre-filter"
+              onChange={(value) => handleChangeGenre(value as Genre)}
+              getOptionLabel={(genre: Genre) => genre.name}
+              getOptionValue={(genre: Genre) => String(genre.id)}
+            />
+          )}
+        />
+      </form>
     </div>
   );
 };
--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -3,31 +3,58 @@ import { useEffect, useState } from 'react';
 import { Movie } from 'types/movie';
 import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from 'utils/requests';
-import GenreFilter from './GenreFilter';
+import GenreFilter, { GenreFilterData } from './GenreFilter';
 import MovieCard from './MovieCard';
-import Pagination from "./Pagination";
+import Pagination from './Pagination';
 
 import './styles.css';
+
+type ControlComponentsData = {
+  activePage: number;
+  filterData: GenreFilterData;
+};
 
 const Movies = () => {
   const [page, setPage] = useState<SpringPage<Movie>>();
 
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+      filterData: { genre: null },
+    });
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({
+      activePage: pageNumber,
+      filterData: controlComponentsData.filterData,
+    });
+  };
+
+  const handleSubmitFilter = (data: GenreFilterData) => {
+    setControlComponentsData({ activePage: 0, filterData: data });
+  };
+
   useEffect(() => {
-    const params: AxiosRequestConfig = {
+    const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/movies',
       withCredentials: true,
+      params: {
+        page: controlComponentsData.activePage,
+        size: 4,
+        genreId: controlComponentsData.filterData.genre?.id,
+      },
     };
 
-    requestBackend(params).then((response) => {
+    requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  }, [controlComponentsData]);
 
   return (
     <div>
       <div className="movies-page-ctr">
-        <GenreFilter />
+        <GenreFilter onSubmitFilter={handleSubmitFilter} />
 
         <div className="list-movies-ctr">
           <div className="row">
